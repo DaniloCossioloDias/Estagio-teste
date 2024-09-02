95b8122234acbbd7cd034a3efb929a58f7c4eaf5
DaniloCossioloDias <116849332+DaniloCossioloDias@users.noreply.github.com>
2024-09-02 17:51:52 -0300

    Add files via upload

---
+++ Ex1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Ex1
+{
+    internal class Fibonacci
+    {
+        static bool EhFibonacci(int numero)
+        {
+            if (numero < 0)
+                return false;
+
+            int a = 0, b = 1;
+            if (numero == a || numero == b)
+                return true;
+
+            while (b < numero)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            return b == numero;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Verificação de Fibonacci\n----------------------------------");
+
+
+            int numero = 21;
+
+            if (EhFibonacci(numero))
+            {
+                Console.WriteLine($"{numero} esta na sequencia de Fibonacci.");
+            }
+            else
+            {
+                Console.WriteLine($"{numero} n�o esta na sequencia de Fibonacci.");
+            }
+        }
+    }
+}
---
+++ Ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string palavra = "plvr";
+            int cont = 0;
+            for (int i = 0; i < palavra.Length; i++) 
+            {
+                if(palavra[i] == 'A' || palavra[i] == 'a')
+                {
+                    cont++;
+                }
+            }
+            Console.WriteLine(cont);
+        }
+
+    }
+}
+
+
